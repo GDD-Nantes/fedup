0a2eb884bddb8284018e6e5e6f86437c7768c0e9
chat-wane <grumpy.chat.wane@gmail.com>
2023-11-30 18:33:00 +0100

    barely nothing

---
+++ src/test/java/fr/gdd/fedup/RSATest.java
@@ -0,0 +1,13 @@
+package fr.gdd.fedup;
+
+/**
+ * Try to create RSA using the identity data of FedShop.
+ * TODO create summaries of other kinds, not only Apache Jena !
+ * TODO since we create SPARQL queries anyway…
+ */
+public class RSATest {
+
+
+
+
+}
