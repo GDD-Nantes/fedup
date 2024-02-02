9354fdba3e4a882af0de9088077482256146953b
chat-wane <grumpy.chat.wane@gmail.com>
2024-02-02 17:36:42 +0100

    allow for multiple summaries

--- src/main/java/fr/gdd/fedup/fuseki/FedUPServer.java
+++ src/main/java/fr/gdd/fedup/fuseki/FedUPServer.java
@@ -3,12 +3,21 @@ package fr.gdd.fedup.fuseki;
 import fr.gdd.fedup.summary.ModuloOnSuffix;
 import fr.gdd.fedup.summary.Summary;
 import org.apache.commons.cli.*;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.jena.dboe.base.file.Location;
 import org.apache.jena.fuseki.main.FusekiServer;
 import org.apache.jena.query.ARQ;
 import org.apache.jena.riot.resultset.ResultSetLang;
 import org.apache.jena.riot.rowset.RowSetWriterRegistry;
 import org.apache.jena.sparql.mgt.Explain;
+import org.checkerframework.checker.units.qual.A;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * FedUP server that runs on top of Apache Jena Fuseki. All options available are
@@ -21,8 +30,13 @@ public class FedUPServer {
 
         options.addOption(new Option("h", "help", false,
                 "print this message"));
-        options.addOption("s", "summary", true,
-                "The path to the summary as TDB2 dataset.");
+
+        Option summariesOpt = new Option("s", "summaries", true,
+                "Path(s) to TDB2 dataset summary(ies).");
+        summariesOpt.setArgs(Option.UNLIMITED_VALUES);
+        summariesOpt.setValueSeparator(',');
+        options.addOption(summariesOpt);
+
         // options.addOption("t", "type", true, "The summary type (example: ModuloOnSuffix(1)).");
         options.addOption("e", "engine", true,
                 "The federation engine in charge of executing (default: Jena; FedX).");
@@ -32,12 +46,13 @@ public class FedUPServer {
                 "The port of this FedUP server (default: 3330).");
         // TODO Add option to configure remote virtuoso
 
+
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
 
         if (cmd.hasOption("help")) {
             HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("fedup [options] --sumary <path>", options);
+            formatter.printHelp("fedup [options] --sumaries <path>", options);
             return;
         }
 
@@ -45,36 +60,38 @@ public class FedUPServer {
         // TODO parse the arg
         // TODO or encode it within the dataset by default.
         ModuloOnSuffix strategy = new ModuloOnSuffix(1);
-        Summary s = new Summary(strategy, Location.create(cmd.getOptionValue("s")));
 
-        // TODO explain level as argument
-        ARQ.setExecutionLogging(Explain.InfoLevel.ALL);
-
-        // Export the results in the HTTP response?
-        s.getSummary().getContext().set(FedUPConstants.EXPORT_PLANS, cmd.hasOption("x"));
-        // Which engine use once the sources are assigned?
-        if (cmd.hasOption("e")) {
-            s.getSummary().getContext().set(FedUPConstants.EXECUTION_ENGINE, cmd.getOptionValue("e"));
-        } else {
-            s.getSummary().getContext().set(FedUPConstants.EXECUTION_ENGINE, FedUPConstants.APACHE_JENA);
+        List<Pair<String, Summary>> summaries = new ArrayList<>();
+        for (Path path: Arrays.stream(cmd.getOptionValues('s')).map(Path::of).toList()) {
+            Summary s = new Summary(strategy, Location.create(path));
+            // Export the results in the HTTP response?
+            s.getSummary().getContext().set(FedUPConstants.EXPORT_PLANS, cmd.hasOption("x"));
+            // Which engine use once the sources are assigned?
+            if (cmd.hasOption("e")) {
+                s.getSummary().getContext().set(FedUPConstants.EXECUTION_ENGINE, cmd.getOptionValue("e"));
+            } else {
+                s.getSummary().getContext().set(FedUPConstants.EXECUTION_ENGINE, FedUPConstants.APACHE_JENA);
+            }
+            s.getSummary().getContext().set(ARQ.optimization, false);
+            summaries.add(new ImmutablePair<>(path.getFileName().toString(), s));
         }
-        // On which port?
-        int port = cmd.hasOption("p") ? Integer.parseInt(cmd.getOptionValue("p")) : 3330;
 
+        ARQ.setExecutionLogging(Explain.InfoLevel.ALL);  // TODO explain level as argument
+        int port = cmd.hasOption("p") ? Integer.parseInt(cmd.getOptionValue("p")) : 3330; // On which port?
         FedUPEngine.register();
-
-        s.getSummary().getContext().set(ARQ.optimization, false);
-
-
         RowSetWriterRegistry.register(ResultSetLang.RS_JSON, FedUPPlanAndNormalJSON.factory);
 
-        FusekiServer.create()
-                .add("summary", s.getSummary()) // make it accessible, when queried, it makes federated query
+        var builder = FusekiServer.create()
                 .port(port)
                 .enableCors(true)
-                .verbose(true)
-                .build()
-                .start();
+                .verbose(true);
+
+        for (Pair<String, Summary> nameAndSummary : summaries) {
+            System.out.println("Summary available: " + nameAndSummary.getLeft());
+            builder.add(nameAndSummary.getLeft(), nameAndSummary.getRight().getSummary());
+        }
+
+        builder.build().start();
     }
 
 }
